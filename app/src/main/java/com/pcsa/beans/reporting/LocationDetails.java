48b88932dde86b21846828df0388bb6de173a2d4
Buddhiprabha <buddhiprabha31481@gmail.com>
2015-08-11 23:15:00 +0530

    Contact Post Staff enabled. Dialr and SMS apps integrated to PCSA

--- app/src/main/java/com/pcsa/beans/reporting/LocationDetails.java
+++ app/src/main/java/com/pcsa/beans/reporting/LocationDetails.java
@@ -7,11 +7,11 @@ package com.pcsa.beans.reporting;
  */
 public class LocationDetails {
     private String locationName;
-    private int pcmo_contact;
-    private int ssm_contact;
-    private int sarl_contact;
+    private String pcmo_contact;
+    private String ssm_contact;
+    private String sarl_contact;
 
-    public LocationDetails(String locationName, int pcmo_contact, int ssm_contact, int sarl_contact){
+    public LocationDetails(String locationName, String pcmo_contact, String ssm_contact, String sarl_contact){
         this.locationName = locationName;
         this.pcmo_contact = pcmo_contact;
         this.ssm_contact = ssm_contact;
@@ -26,27 +26,27 @@ public class LocationDetails {
         this.locationName = locationName;
     }
 
-    public int getPcmo_contact() {
+    public String getPcmo_contact() {
         return pcmo_contact;
     }
 
-    public void setPcmo_contact(int pcmo_contact) {
+    public void setPcmo_contact(String pcmo_contact) {
         this.pcmo_contact = pcmo_contact;
     }
 
-    public int getSsm_contact() {
+    public String getSsm_contact() {
         return ssm_contact;
     }
 
-    public void setSsm_contact(int ssm_contact) {
+    public void setSsm_contact(String ssm_contact) {
         this.ssm_contact = ssm_contact;
     }
 
-    public int getSarl_contact() {
+    public String getSarl_contact() {
         return sarl_contact;
     }
 
-    public void setSarl_contact(int sarl_contact) {
+    public void setSarl_contact(String sarl_contact) {
         this.sarl_contact = sarl_contact;
     }
 }
--- app/src/main/java/com/pcsa/views/reporting/ContactPostStaff.java
+++ app/src/main/java/com/pcsa/views/reporting/ContactPostStaff.java
@@ -1,7 +1,12 @@
 package com.pcsa.views.reporting;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.ContextThemeWrapper;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -27,13 +32,15 @@ public class ContactPostStaff extends Activity implements AdapterView.OnItemSele
     Button contact_ssm;
     Button contact_sarl;
     TextView current_location;
+
+    LocationDetails selectedLocationDetails;
     private static final Map<String, LocationDetails> locationDetails;
     static {
         locationDetails = new HashMap<>();
         //TODO: Add correct values here
-        locationDetails.put("Uganda", new LocationDetails("Uganda", 1111, 2222, 3333));
-        locationDetails.put("Syria", new LocationDetails("Syria", 4444, 5555, 6666));
-        locationDetails.put("Tunisia", new LocationDetails("Tunisia",7777, 8888, 9999));
+        locationDetails.put("Uganda", new LocationDetails("Uganda", "1111", "2222", "3333"));
+        locationDetails.put("Syria", new LocationDetails("Syria", "4444", "5555", "6666"));
+        locationDetails.put("Tunisia", new LocationDetails("Tunisia","7777", "8888", "9999"));
     }
 
     @Override
@@ -50,6 +57,27 @@ public class ContactPostStaff extends Activity implements AdapterView.OnItemSele
         contact_ssm.setText(R.string.contact_ssm);
         contact_sarl.setText(R.string.contact_sarl);
 
+        contact_pcmo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createDialog(v, selectedLocationDetails.getPcmo_contact());
+            }
+        });
+
+        contact_ssm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createDialog(v, selectedLocationDetails.getSsm_contact());
+            }
+        });
+
+        contact_sarl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createDialog(v, selectedLocationDetails.getSarl_contact());
+            }
+        });
+
         Spinner locationList = (Spinner) findViewById(R.id.reporting_locationlist);
         locationList.setOnItemSelectedListener(this);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
@@ -58,13 +86,51 @@ public class ContactPostStaff extends Activity implements AdapterView.OnItemSele
         locationList.setAdapter(adapter);
     }
 
+    /**
+     * Creates a Dialog for the user to choose Dialer app or SMS app
+     * @param v the view clicked
+     * @param numberToContact contact number corresponding to the view
+     */
+    private void createDialog(View v, final String numberToContact){
+        DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                contactStaff(which, numberToContact);
+            }
+        };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(new ContextThemeWrapper(v.getContext(), R.style.pcsaAlertDialogStyle));
+        builder.setMessage("Contact Via").setPositiveButton("Voice Call", dialogClickListener)
+                .setNegativeButton("Send Message", dialogClickListener).show();
+    }
+
+    /**
+     * Opens Dialer or SMS
+     * @param action which app to open
+     * @param contactNumber the contact number of the selected person
+     */
+    private void contactStaff(int action, String contactNumber){
+        switch (action){
+            case DialogInterface.BUTTON_POSITIVE:
+                Intent callingIntent = new Intent(Intent.ACTION_CALL);
+                callingIntent.setData(Uri.parse("tel:"+contactNumber));
+                startActivity(callingIntent);
+                break;
+
+            case DialogInterface.BUTTON_NEGATIVE:
+                Intent smsIntent = new Intent(Intent.ACTION_VIEW);
+                smsIntent.setData(Uri.parse("sms:"+contactNumber));
+                startActivity(smsIntent);
+        }
+    }
+
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         String selectedItem = (String) parent.getItemAtPosition(position);
         current_location.setText(getResources().getString(R.string.reporting_current_location) + " " + selectedItem);
 
         // selectedLocationDetails holds all details about the location selected by the user
-        LocationDetails selectedLocationDetails = locationDetails.get(selectedItem);
+        selectedLocationDetails = locationDetails.get(selectedItem);
     }
 
     @Override
