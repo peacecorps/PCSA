156ef0e5697bdc4f9f74da0b00104699314e6814
Minerva Panda <minervapandaniki@gmail.com>
2016-04-06 15:33:57 +0530

    added comments to safety resources

--- app/src/main/java/com/peacecorps/pcsa/safety_resources/SafetyResourcesFragment.java
+++ app/src/main/java/com/peacecorps/pcsa/safety_resources/SafetyResourcesFragment.java
@@ -8,18 +8,28 @@ import android.view.ViewGroup;
 
 import com.peacecorps.pcsa.R;
 
-
-/**
- * A placeholder fragment containing a simple view.
+/*
+ * Safety Resources main fragment 
+ *
+ * @author calistus
+ * @since 2015-08-18
  */
 public class SafetyResourcesFragment extends Fragment {
 
     public SafetyResourcesFragment() {
     }
-
-    @Override
+    /**
+     * Create the view for this fragment, using the arguments given to it. 
+     * 
+     * @param inflater inflate any views in the fragment
+     * @param container if non-null, this is the parent view that the fragment's UI should be attached to. .
+     * @param savedInstanceState If non-null, this fragment is being re-constructed from a previous saved state 
+     * @return the properly constructed view object
+     */
+     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        // Inflate the layout for this fragment                         
         return inflater.inflate(R.layout.fragment_safety_resources, container, false);
     }
 }
