9020a7e504a81a8b5af0d2adf90f19bad5ba8244
rohands <rohan0495@gmail.com>
2016-06-02 20:02:55 +0530

    UI Changes implemented

---
+++ app/src/main/java/com/peacecorps/pcsa/SplashScreenActivity.java
@@ -0,0 +1,46 @@
+package com.peacecorps.pcsa;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ProgressBar;
+
+/**
+ * Created by rohan on 02-06-2016.
+ */
+public class SplashScreenActivity extends Activity {
+
+    // Splash screen timer
+    private static int SPLASH_TIME_OUT = 2500;
+    private ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+        progressBar = (ProgressBar)findViewById(R.id.splash_screen_progress);
+        progressBar.setVisibility(View.VISIBLE);
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                progressBar.setVisibility(View.GONE);
+                Intent i = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+
+}
